d782741af7478266fd284ae89cf0ddef3ff02555
daktak <daktak@domo.ddns.net>
2026-09-06 13:33:15 +1000

    sleep score

--- src/c/main.c
+++ src/c/main.c
@@ -10,11 +10,13 @@
 #define KEY_QUEUED_Y_SLEEP 22
 #define KEY_QUEUED_Y_RHR 23
 #define KEY_QUEUED_Y_SHR 24
+#define KEY_QUEUED_Y_SCORE 30
 #define KEY_QUEUED_T_DATE 25
 #define KEY_QUEUED_T_STEPS 26
 #define KEY_QUEUED_T_SLEEP 27
 #define KEY_QUEUED_T_RHR 28
 #define KEY_QUEUED_T_SHR 29
+#define KEY_QUEUED_T_SCORE 31
 
 static Window *s_window;
 static TextLayer *s_time_layer;
@@ -22,12 +24,14 @@ static TextLayer *s_steps_layer;
 static TextLayer *s_sleep_layer;
 static TextLayer *s_rhr_layer;
 static TextLayer *s_shr_layer;
+static TextLayer *s_score_layer;
 static TextLayer *s_status_layer;
 static char s_time_buf[16];
 static char s_steps_buf[32];
 static char s_sleep_buf[32];
 static char s_rhr_buf[32];
 static char s_shr_buf[32];
+static char s_score_buf[32];
 static char s_status_buf[64];
 static char s_y_date[12];
 static char s_t_date[12];
@@ -61,11 +65,38 @@ static void format_sleep(int secs, char *buf, size_t len) {
   else snprintf(buf, len, "%dh%02dm sleep", secs / 3600, (secs % 3600) / 60);
 }
 
+static int calc_sleep_score(int total, int restful, int rhr, int shr) {
+  if (total <= 0) return 0;
+  int durationScore = total * 100 / 28800;
+  if (durationScore > 100) durationScore = 100;
+  int restfulScore;
+  if (restful <= 0) restfulScore = 50;
+  else {
+    int ratioPct = restful * 100 / total;
+    restfulScore = ratioPct * 100 / 25;
+    if (restfulScore > 100) restfulScore = 100;
+  }
+  int hrScore;
+  if (rhr <= 0 || shr <= 0) hrScore = 50;
+  else {
+    int delta = rhr - shr;
+    int add = delta * 500 / rhr;
+    hrScore = 60 + add;
+    if (hrScore < 0) hrScore = 0;
+    if (hrScore > 100) hrScore = 100;
+  }
+  int score = (durationScore * 40 + restfulScore * 35 + hrScore * 25) / 100;
+  if (score < 0) score = 0;
+  if (score > 100) score = 100;
+  return score;
+}
+
 typedef struct {
   int steps;
   int sleep;
   int rhr;
   int shr;
+  int sleepScore;
   char date[12];
 } WellnessDay;
 
@@ -81,6 +112,7 @@ static void update_display(void) {
     text_layer_set_text(s_sleep_layer, "");
     text_layer_set_text(s_rhr_layer, "");
     text_layer_set_text(s_shr_layer, "");
+    text_layer_set_text(s_score_layer, "");
     return;
   }
   int steps = get_steps_today();
@@ -99,10 +131,12 @@ static void update_display(void) {
       s_cached_y.shr = persist_read_int(KEY_QUEUED_Y_SHR);
       s_cached_y.steps = persist_read_int(KEY_QUEUED_Y_STEPS);
       s_cached_y.sleep = persist_read_int(KEY_QUEUED_Y_SLEEP);
+      if (persist_exists(KEY_QUEUED_Y_SCORE)) s_cached_y.sleepScore = persist_read_int(KEY_QUEUED_Y_SCORE);
       if (persist_exists(KEY_QUEUED_T_DATE)) {
         persist_read_string(KEY_QUEUED_T_DATE, s_cached_t.date, sizeof(s_cached_t.date));
         s_cached_t.rhr = persist_read_int(KEY_QUEUED_T_RHR);
         s_cached_t.shr = persist_read_int(KEY_QUEUED_T_SHR);
+        if (persist_exists(KEY_QUEUED_T_SCORE)) s_cached_t.sleepScore = persist_read_int(KEY_QUEUED_T_SCORE);
       }
       s_has_cache = true;
     } else {
@@ -120,6 +154,9 @@ static void update_display(void) {
   else snprintf(s_shr_buf, sizeof(s_shr_buf), "Avg Sleep HR --");
   text_layer_set_text(s_rhr_layer, s_rhr_buf);
   text_layer_set_text(s_shr_layer, s_shr_buf);
+  if (s_cached_y.sleepScore > 0) snprintf(s_score_buf, sizeof(s_score_buf), "Sleep Score %d", s_cached_y.sleepScore);
+  else snprintf(s_score_buf, sizeof(s_score_buf), "Sleep Score --");
+  text_layer_set_text(s_score_layer, s_score_buf);
 #endif
 }
 
@@ -142,6 +179,9 @@ static int query_day(time_t start, time_t end, WellnessDay *out) {
   if (m & HealthServiceAccessibilityMaskAvailable) out->steps = (int)health_service_sum(HealthMetricStepCount, start, end);
   m = health_service_metric_accessible(HealthMetricSleepSeconds, start, end);
   if (m & HealthServiceAccessibilityMaskAvailable) out->sleep = (int)health_service_sum(HealthMetricSleepSeconds, start, end);
+  int restful = 0;
+  m = health_service_metric_accessible(HealthMetricSleepRestfulSeconds, start, end);
+  if (m & HealthServiceAccessibilityMaskAvailable) restful = (int)health_service_sum(HealthMetricSleepRestfulSeconds, start, end);
   {
     int hr_min = 255;
     int hr_sum = 0;
@@ -180,7 +220,9 @@ static int query_day(time_t start, time_t end, WellnessDay *out) {
         }
       }
     }
-    APP_LOG(APP_LOG_LEVEL_DEBUG, "query %s hr_min %d avg %d cnt %d", out->date, out->rhr, out->shr, hr_count);
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "query %s hr_min %d avg %d cnt %d restful %d", out->date, out->rhr, out->shr, hr_count, restful);
+    out->sleepScore = calc_sleep_score(out->sleep, restful, out->rhr, out->shr);
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "score %s %d dur %d rest %d", out->date, out->sleepScore, out->sleep, restful);
   }
 #else
   (void)start; (void)end;
@@ -199,11 +241,13 @@ static void send_queued(void) {
   if (persist_exists(KEY_QUEUED_Y_SLEEP)) y.sleep = persist_read_int(KEY_QUEUED_Y_SLEEP);
   if (persist_exists(KEY_QUEUED_Y_RHR)) y.rhr = persist_read_int(KEY_QUEUED_Y_RHR);
   if (persist_exists(KEY_QUEUED_Y_SHR)) y.shr = persist_read_int(KEY_QUEUED_Y_SHR);
+  if (persist_exists(KEY_QUEUED_Y_SCORE)) y.sleepScore = persist_read_int(KEY_QUEUED_Y_SCORE);
   if (persist_exists(KEY_QUEUED_T_DATE)) persist_read_string(KEY_QUEUED_T_DATE, tt.date, sizeof(tt.date));
   if (persist_exists(KEY_QUEUED_T_STEPS)) tt.steps = persist_read_int(KEY_QUEUED_T_STEPS);
   if (persist_exists(KEY_QUEUED_T_SLEEP)) tt.sleep = persist_read_int(KEY_QUEUED_T_SLEEP);
   if (persist_exists(KEY_QUEUED_T_RHR)) tt.rhr = persist_read_int(KEY_QUEUED_T_RHR);
   if (persist_exists(KEY_QUEUED_T_SHR)) tt.shr = persist_read_int(KEY_QUEUED_T_SHR);
+  if (persist_exists(KEY_QUEUED_T_SCORE)) tt.sleepScore = persist_read_int(KEY_QUEUED_T_SCORE);
   if (y.date[0] == '\0' && tt.date[0] == '\0') return;
   s_cached_y = y;
   s_cached_t = tt;
@@ -219,6 +263,7 @@ static void queue_wellness(WellnessDay *y, WellnessDay *t) {
     persist_write_int(KEY_QUEUED_Y_SLEEP, y->sleep);
     persist_write_int(KEY_QUEUED_Y_RHR, y->rhr);
     persist_write_int(KEY_QUEUED_Y_SHR, y->shr);
+    persist_write_int(KEY_QUEUED_Y_SCORE, y->sleepScore);
   } else {
     persist_delete(KEY_QUEUED_Y_DATE);
   }
@@ -228,6 +273,7 @@ static void queue_wellness(WellnessDay *y, WellnessDay *t) {
     persist_write_int(KEY_QUEUED_T_SLEEP, t->sleep);
     persist_write_int(KEY_QUEUED_T_RHR, t->rhr);
     persist_write_int(KEY_QUEUED_T_SHR, t->shr);
+    persist_write_int(KEY_QUEUED_T_SCORE, t->sleepScore);
   } else {
     persist_delete(KEY_QUEUED_T_DATE);
   }
@@ -247,6 +293,7 @@ static void send_wellness(WellnessDay *y, WellnessDay *t) {
     dict_write_int(out, MESSAGE_KEY_Y_SLEEP, &y->sleep, sizeof(y->sleep), true);
     dict_write_int(out, MESSAGE_KEY_Y_RHR, &y->rhr, sizeof(y->rhr), true);
     dict_write_int(out, MESSAGE_KEY_Y_SHR, &y->shr, sizeof(y->shr), true);
+    dict_write_int(out, MESSAGE_KEY_Y_SCORE, &y->sleepScore, sizeof(y->sleepScore), true);
     dict_write_cstring(out, MESSAGE_KEY_Y_DATE, y->date);
   }
   if (t) {
@@ -254,6 +301,7 @@ static void send_wellness(WellnessDay *y, WellnessDay *t) {
     dict_write_int(out, MESSAGE_KEY_T_SLEEP, &t->sleep, sizeof(t->sleep), true);
     dict_write_int(out, MESSAGE_KEY_T_RHR, &t->rhr, sizeof(t->rhr), true);
     dict_write_int(out, MESSAGE_KEY_T_SHR, &t->shr, sizeof(t->shr), true);
+    dict_write_int(out, MESSAGE_KEY_T_SCORE, &t->sleepScore, sizeof(t->sleepScore), true);
     dict_write_cstring(out, MESSAGE_KEY_T_DATE, t->date);
   }
   int32_t cmd = 0;
@@ -480,7 +528,13 @@ static void window_load(Window *window) {
   text_layer_set_font(s_shr_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
   text_layer_set_text_alignment(s_shr_layer, GTextAlignmentCenter);
   layer_add_child(root, text_layer_get_layer(s_shr_layer));
-  s_status_layer = text_layer_create(GRect(5, bounds.size.h - 42, bounds.size.w - 10, 18));
+  s_score_layer = text_layer_create(GRect(0, 110, w, 18));
+  text_layer_set_background_color(s_score_layer, GColorClear);
+  text_layer_set_text_color(s_score_layer, GColorLightGray);
+  text_layer_set_font(s_score_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
+  text_layer_set_text_alignment(s_score_layer, GTextAlignmentCenter);
+  layer_add_child(root, text_layer_get_layer(s_score_layer));
+  s_status_layer = text_layer_create(GRect(5, bounds.size.h - 38, bounds.size.w - 10, 18));
   text_layer_set_background_color(s_status_layer, GColorClear);
   text_layer_set_text_color(s_status_layer, GColorLightGray);
   text_layer_set_font(s_status_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14));
@@ -529,6 +583,7 @@ static void window_unload(Window *window) {
   text_layer_destroy(s_sleep_layer);
   text_layer_destroy(s_rhr_layer);
   text_layer_destroy(s_shr_layer);
+  text_layer_destroy(s_score_layer);
   text_layer_destroy(s_status_layer);
 }
 
