7837603a0d1d3d34370f7c3d8ca639460b4467ae
daktak <daktak@domo.ddns.net>
2026-09-18 09:53:09 +1000

    worker fix

--- worker_src/c/worker.c
+++ worker_src/c/worker.c
@@ -11,8 +11,8 @@
 #define KEY_HRV_RING_RMSSD 52
 #define KEY_HRV_RING_SDNN 53
 
-#define PPI_BUF_SIZE 256
-#define BURST_BUF_SIZE 200
+#define PPI_BUF_SIZE 64
+#define BURST_BUF_SIZE 50
 
 static bool s_hrv_sampling = false;
 static uint16_t s_ppi_buf[PPI_BUF_SIZE];
@@ -22,19 +22,41 @@ static int s_burst_sdnn[BURST_BUF_SIZE];
 static int s_burst_cnt = 0;
 static bool s_was_active = false;
 
+// Static date buffers to avoid stack allocation
+static char s_today_buf[12];
+static char s_nightly_buf[12];
+static int s_tmp_rmssd[BURST_BUF_SIZE];
+static int s_tmp_sdnn[BURST_BUF_SIZE];
+static char s_date_buf[12];
+
+static int quickselect_median(int *arr, int n);
+
+static void worker_log(const char *msg) {
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "worker: %s", msg);
+}
+
+static void worker_log_int(const char *msg, int val) {
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "worker: %s %d", msg, val);
+}
+
 static bool hrv_window_active(void) {
+  worker_log("hrv_window_active start");
   int sh = 22; int sm = 0; int eh = 8; int em = 0;
+  worker_log("hrv_window_active persist read start");
   if (persist_exists(KEY_HRV_START_HOUR)) sh = persist_read_int(KEY_HRV_START_HOUR);
   if (persist_exists(KEY_HRV_START_MINUTE)) sm = persist_read_int(KEY_HRV_START_MINUTE);
   if (persist_exists(KEY_HRV_END_HOUR)) eh = persist_read_int(KEY_HRV_END_HOUR);
   if (persist_exists(KEY_HRV_END_MINUTE)) em = persist_read_int(KEY_HRV_END_MINUTE);
+  worker_log("hrv_window_active persist read done");
   if (sh < 0 || sh > 23) sh = 22;
   if (eh < 0 || eh > 23) eh = 8;
   if (sm < 0 || sm > 59) sm = 0;
   if (em < 0 || em > 59) em = 0;
   if (sh == eh && sm == em) return false;
+  worker_log("hrv_window_active time start");
   time_t now = time(NULL);
   struct tm *t = localtime(&now);
+  worker_log("hrv_window_active time done");
   int cur = t->tm_hour * 60 + t->tm_min;
   int start = sh * 60 + sm;
   int end = eh * 60 + em;
@@ -43,7 +65,13 @@ static bool hrv_window_active(void) {
 }
 
 static bool duty_active(void) {
-  if (!hrv_window_active()) return false;
+  worker_log("duty_active start");
+  if (!hrv_window_active()) {
+    worker_log("duty_active hrv_window_active false");
+    worker_log("duty_active returning false");
+    return false;
+  }
+  worker_log("duty_active hrv_window_active true");
   time_t now = time(NULL);
   struct tm *t = localtime(&now);
   int mins = t->tm_hour * 60 + t->tm_min;
@@ -62,6 +90,7 @@ static void format_date(time_t t, char *buf, size_t len) {
 }
 
 static void calc_rmssd_sdnn(uint16_t *buf, int n, int *out_rmssd, int *out_sdnn) {
+  worker_log_int("calc_rmssd_sdnn n", n);
   *out_rmssd = 0; *out_sdnn = 0;
   if (n < 2) return;
   int sum_diff_sq = 0;
@@ -91,6 +120,8 @@ static void calc_rmssd_sdnn(uint16_t *buf, int n, int *out_rmssd, int *out_sdnn)
 }
 
 static void burst_end(void) {
+  worker_log_int("burst_end ppi_cnt", s_ppi_cnt);
+  worker_log_int("burst_end burst_cnt", s_burst_cnt);
   if (s_ppi_cnt < 2) { s_ppi_cnt = 0; return; }
   int rmssd, sdnn;
   calc_rmssd_sdnn(s_ppi_buf, s_ppi_cnt, &rmssd, &sdnn);
@@ -106,20 +137,45 @@ static void burst_end(void) {
 }
 
 static void night_end(void) {
+  worker_log_int("night_end burst_cnt", s_burst_cnt);
   if (s_burst_cnt == 0) return;
-  int tmp_rmssd[BURST_BUF_SIZE];
-  int tmp_sdnn[BURST_BUF_SIZE];
-  for (int i = 0; i < s_burst_cnt; i++) { tmp_rmssd[i] = s_burst_rmssd[i]; tmp_sdnn[i] = s_burst_sdnn[i]; }
-  for (int i = 0; i < s_burst_cnt; i++) for (int j = i+1; j < s_burst_cnt; j++) if (tmp_rmssd[j] < tmp_rmssd[i]) { int t = tmp_rmssd[i]; tmp_rmssd[i] = tmp_rmssd[j]; tmp_rmssd[j] = t; }
-  for (int i = 0; i < s_burst_cnt; i++) for (int j = i+1; j < s_burst_cnt; j++) if (tmp_sdnn[j] < tmp_sdnn[i]) { int t = tmp_sdnn[i]; tmp_sdnn[i] = tmp_sdnn[j]; tmp_sdnn[j] = t; }
-  int median_rmssd = tmp_rmssd[s_burst_cnt/2];
-  int median_sdnn = tmp_sdnn[s_burst_cnt/2];
-  char date[12];
-  format_date(time(NULL), date, sizeof(date));
+  worker_log("night_end copy");
+  for (int i = 0; i < s_burst_cnt; i++) {
+    s_tmp_rmssd[i] = s_burst_rmssd[i];
+    s_tmp_sdnn[i] = s_burst_sdnn[i];
+  }
+  worker_log("night_end quickselect");
+  int median_rmssd = quickselect_median(s_tmp_rmssd, s_burst_cnt);
+  int median_sdnn = quickselect_median(s_tmp_sdnn, s_burst_cnt);
+  worker_log("night_end persist_write");
   persist_write_int(KEY_HRV_NIGHT_RMSSD, median_rmssd);
   persist_write_int(KEY_HRV_NIGHT_SDNN, median_sdnn);
-  persist_write_string(KEY_HRV_NIGHT_DATE, date);
   s_burst_cnt = 0;
+  worker_log("night_end done");
+}
+
+// Quickselect median - O(n) average, minimal stack
+static int quickselect_median(int *arr, int n) {
+  int k = n / 2;
+  int left = 0, right = n - 1;
+  while (left < right) {
+    int pivot = arr[(left + right) / 2];
+    int i = left, j = right;
+    while (i <= j) {
+      while (arr[i] < pivot) i++;
+      while (arr[j] > pivot) j--;
+      if (i <= j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+        i++; j--;
+      }
+    }
+    if (k <= j) right = j;
+    else if (k >= i) left = i;
+    else break;
+  }
+  return arr[k];
 }
 
 static void hrv_event_handler(HealthEventType event, void *ctx) {
@@ -127,11 +183,16 @@ static void hrv_event_handler(HealthEventType event, void *ctx) {
     uint16_t ppi = health_service_peek_hrv_ppi_ms();
     if (ppi > 250 && ppi < 2200 && s_ppi_cnt < PPI_BUF_SIZE) {
       s_ppi_buf[s_ppi_cnt++] = ppi;
+      if (s_ppi_cnt % 50 == 0) {
+        worker_log_int("hrv_event ppi", ppi);
+        worker_log_int("hrv_event cnt", s_ppi_cnt);
+      }
     }
   }
 }
 
 static void restore_bursts(void) {
+  worker_log("restore_bursts");
   if (!persist_exists(KEY_HRV_RING_CNT)) return;
   int cnt = persist_read_int(KEY_HRV_RING_CNT);
   if (cnt <= 0 || cnt > BURST_BUF_SIZE) return;
@@ -144,47 +205,65 @@ static void restore_bursts(void) {
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  worker_log_int("tick_handler hour", tick_time->tm_hour);
+  worker_log_int("tick_handler min", tick_time->tm_min);
+  worker_log("tick_handler hrv_window_active");
   bool active = hrv_window_active();
+  worker_log_int("tick_handler active", active);
+  worker_log("tick_handler duty_active");
   bool duty = duty_active();
+  worker_log_int("tick_handler duty", duty);
   bool should_sample = active && duty;
+  worker_log_int("tick_handler should_sample", should_sample);
+  worker_log_int("tick_handler s_hrv_sampling", s_hrv_sampling);
   if (should_sample && !s_hrv_sampling) {
+    worker_log("tick_handler starting sampling");
     health_service_set_hrv_sample_period(30);
     s_hrv_sampling = true;
     s_ppi_cnt = 0;
   } else if (!should_sample && s_hrv_sampling) {
+    worker_log("tick_handler stopping sampling");
     health_service_set_hrv_sample_period(0);
     s_hrv_sampling = false;
     burst_end();
   }
+  worker_log_int("tick_handler s_was_active", s_was_active);
+  worker_log_int("tick_handler s_burst_cnt", s_burst_cnt);
   if (!active && s_was_active) {
+    worker_log("tick_handler branch 1: !active && s_was_active");
     if (s_ppi_cnt > 0) burst_end();
     night_end();
   } else if (!active && !s_was_active && s_burst_cnt == 0) {
+    worker_log("tick_handler branch 2: !active && !s_was_active && s_burst_cnt == 0");
     restore_bursts();
     if (s_burst_cnt > 0) {
       if (s_ppi_cnt > 0) burst_end();
       night_end();
     }
   } else if (!active && s_burst_cnt > 0) {
-    char today[12]; format_date(time(NULL), today, sizeof(today));
-    char nightly[12] = {0};
-    if (persist_exists(KEY_HRV_NIGHT_DATE)) persist_read_string(KEY_HRV_NIGHT_DATE, nightly, sizeof(nightly));
-    if (strcmp(nightly, today) != 0) {
-      if (s_ppi_cnt > 0) burst_end();
-      night_end();
-    }
+    worker_log("tick_handler branch 3 start");
+    worker_log("tick_handler branch 3 calling night_end");
+    if (s_ppi_cnt > 0) burst_end();
+    night_end();
+    worker_log("tick_handler branch 3 done");
   }
+  worker_log("tick_handler end");
   s_was_active = active;
 }
 
 int main(void) {
+  worker_log("main start");
 #if PBL_API_EXISTS(health_service_peek_hrv_ppi_ms)
   restore_bursts();
+  worker_log("after restore_bursts");
   health_service_events_subscribe(hrv_event_handler, NULL);
+  worker_log("after health_service_events_subscribe");
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  worker_log("after tick_timer_service_subscribe");
   time_t now = time(NULL);
   struct tm *t = localtime(&now);
   tick_handler(t, MINUTE_UNIT);
+  worker_log("after initial tick_handler");
   worker_event_loop();
 #else
   worker_event_loop();
